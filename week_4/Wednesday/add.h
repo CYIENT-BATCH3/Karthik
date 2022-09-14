2df4018e222d3943b4a6f1bf42fbe7014ce8c6ea
KARTHIK V <111974931+Karthik25112@users.noreply.github.com>
2022-09-14 12:47:42 +0530

    Delete week_4/Wednesday directory

--- week_4/Wednesday/add.h
+++
@@ -1,7 +0,0 @@
-void add(int a,int b)		//function define
-{
-	setbuf(stdout,NULL);
-	int sum;		//declaring variabe
-	sum = a+b;	//adding two numbers
-	printf("\naddtion of two number : %d",sum);		//printing output
-}
--- week_4/Wednesday/mul.h
+++
@@ -1,7 +0,0 @@
-void mul(int a,int b)		//function define
-{
-	setbuf(stdout,NULL);
-	int sum2;				//declaration of variable
-	sum2 = a*b; 			//mul of two num
-	printf("\nmul of two number : %d",sum2);		//printing output
-}
